0f71ccb1e16dc19ccab4e98dee4e7047313f116f
Deepak Juneja <93971793+Deepakjuneja28@users.noreply.github.com>
2022-10-06 17:40:53 +0530

    title change

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
 
     <div >
+       <Helmet>
+                <meta charSet="utf-8" />
+                <title>Deepak juneja</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+                <meta name="description" content="Portfolio website" />
+        </Helmet>
       <NavBar/>
       <Home /> 
       <About />
